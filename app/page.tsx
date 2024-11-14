f7d55b00e30bc7e3ef2bc88b37ee3c42b052a089
Régi GOUALE <regi.gouale@gmail.com>
2024-11-14 10:14:06 +0100

    Ajout d'un bouton pour ajouter des membres, mise à jour du titre de la page, tri des personnes par nom de famille, et création d'un hook pour détecter les appareils mobiles. Modification de l'affichage du nom complet en majuscules.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { getPersonsAction } from "@/actions/get-persons-action";
+import { AddPersonButton } from "@/components/persons/add-person-button";
 import { PersonsTable } from "@/components/persons/table";
 import { Person } from "@prisma/client";
 
@@ -12,7 +13,12 @@ export default async function Home() {
   return (
     <div className="h-full">
       <main>
-        <h1 className="text-center text-3xl font-bold">Persons</h1>
+        <div className="mx-auto my-10 flex max-w-4xl items-center justify-between p-4">
+          <h1 className="text-center font-lato text-4xl font-black">
+            Liste des membres
+          </h1>
+          <AddPersonButton />
+        </div>
         <PersonsTable persons={persons} />
       </main>
     </div>
--- src/actions/get-persons-action.ts
+++ src/actions/get-persons-action.ts
@@ -5,7 +5,11 @@ import { Person } from "@prisma/client";
 
 export async function getPersonsAction(): Promise<Person[] | Error> {
   try {
-    const allPersons = await prisma.person.findMany();
+    const allPersons = await prisma.person.findMany({
+      orderBy: {
+        lastname: "asc",
+      },
+    });
     return JSON.parse(JSON.stringify(allPersons)) as Person[];
   } catch (error) {
     console.error(error);
